8ff49102fd727c9d577b91a9d0b1f325f49e6530
Alexandre <aas@unochapeco.edu.br>
2019-01-30 09:01:04 -0200

    inclusao dos fontes

---
+++ app/src/main/java/com/alexandre/modelodelayout/ModeloDeLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.alexandre.modelodelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModeloDeLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modelo_de_layout);
+    }
+}
